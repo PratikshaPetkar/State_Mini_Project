1bc960f941e2c9a39d800297caae8620bb9e1362
pmil2 <pmil2@allstate.com>
2022-09-17 17:07:59 +0530

    component segregation

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,13 @@
-import React, { useState } from "react"; //importing react to make compiler understand that JSX written.
-//importing useState to track state in a fucntion component
+import React from 'react';
 
-const App = () => {
-  //created arrow function
-  let time = new Date().toLocaleTimeString(); // method to get current time in JS
-  const [ctime, setCtime] = useState(time);
-  //ctime - current time
-  //setCtime - updated time
-  //time-->ctime--> <--setCtime -->initial data will get passed to current and whenever there is updated data will also get passed to current and we will get current time
+import Clock from './Clock';
 
-  const UpdateTime = () => {                  // fun to change time after click,
-    time = new Date().toLocaleTimeString();   // method to get current time
-    setCtime(time);       // updated data will get reflect in time through setCtime.
-  };
-  setInterval(UpdateTime, 1000);  //after removing button it should get updated after 1 sec
-                                 //updateTime will get after 1000ms
-  return (       
-    <div>  
-      <h1>{ctime}</h1>                          
-      {/* <button onClick={UpdateTime}> Get Time </button>  */}  
-    </div>
-  );
-};
-
-export default App; // so that index.js file should import this file
-
-// first changed time using onclick event on button on line 21.
+class App extends React.Component{
+  render(){
+    return (
+    <Clock/>
+    )
+   
+  }
+}
+export default App;
